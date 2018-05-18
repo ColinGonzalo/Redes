b9da0cb668e5af52c966c699cf4dee9d15d4df62
Usuario Local <usuario_local@localhost.localdomain>
2018-05-18 12:51:29 +0200

    operator==

--- Redes_2.3/replicacion/Socket.cc
+++ Redes_2.3/replicacion/Socket.cc
@@ -17,6 +17,14 @@
  */
 bool operator== (const Socket &s1, const Socket &s2)
 {
+ if(s1.sa.sa_family == AF_INET && s2.sa.sa_family == AF_INET){
+	struct sockaddr_in *sock_in1 = (struct sockaddr_in *) s1.sa;
+        struct sockaddr_in *sock_in2 = (struct sockaddr_in *) s2.sa;
+
+    return(    sock_in1->sin_family == sock_in2->sin_family &&
+		   sock_in1->sin_port == sock_in2->sin_port &&
+	sock_in1->sin_addr.s_addr == sock_in2->sin_addr.s_addr );
+ }
 
 }
 
@@ -45,13 +53,32 @@ int Socket::bind()
 }
 
 // ----------------------------------------------------------------------------
-
+/**
+     *  Envía un mensaje de aplicación definido por un objeto Serializable.
+     *  NOTA:La función espera que el objeto esté serializado.
+     *
+     *    @param obj en el que se enviará por la red
+     *
+     *    @param sock con la dirección y puerto destino
+     *
+     *    @return 0 en caso de éxito o -1 si error
+     */
 int Socket::send(Serializable * obj, Socket * sock)
 {
 }
 
 // ----------------------------------------------------------------------------
-
+ /**
+     *  Recibe un mensaje de aplicación
+     *
+     *    @param obj buffer para recibir los datos de la red. Se usará para la
+     *    reconstrucción del objeto mediante Serializable::from_bin del interfaz.
+     *    El buffer debe ser de tamaño MAX_MESSAGE_SIZE.
+     *
+     *    @param sock que identificará al extremo que envía los datos si es
+     *    distinto de 0 se creará un objeto Socket con la dirección y puerto.
+     *
+     *    @return 0 en caso de éxito o -1 si error (cerrar conexión)
 int Socket::recv(char * buffer, Socket ** sock)
 {
 }
